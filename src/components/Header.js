8ccfc6353574452b55b1c68092049c7a24059784
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2023-08-16 18:45:28 +0300

    components

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="/img/logo.png" alt="" />
+        <div>
+          <h3 className="text-uppercase">React Sneackers</h3>
+          <p className="opacity-5">Магазин лучших кросовок</p>
+        </div>
+      </div>
+
+      <ul className="d-flex">
+        <li className="mr-30">
+          <img width={18} height={18} src="/img/cart.svg" alt="" />
+
+          <span>1205 $</span>
+        </li>
+        <li>
+          <img width={18} height={18} src="/img/user.svg" alt="" />
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
